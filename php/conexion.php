f8816bae2a6462e2606461e89c36186c1e273d7b
Lizzardi <120577613+Lizzardi@users.noreply.github.com>
2025-05-24 19:51:27 -0500

    php

--- php/conexion.php
+++ php/conexion.php
@@ -1,15 +1,17 @@
-// Este archivo se encargará de la conexión a la base de datos
 <?php
+// Este archivo se encargará de la conexión a la base de datos
 $host = "localhost";
 $port = "5432";
 $dbname = "Gestion_Cine"; 
-$user = "tu_usuario";
-$password = "tu_contraseña";
+$user = "postgres";
+$password = "";
 
 $conn = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$password");
 
 if (!$conn) {
     die("Conexión fallida: " . pg_last_error());
+} else {
+    echo "Conexión exitosa a la base de datos.";
 }
 
 // Opcional: para establecer el charset a utf8
